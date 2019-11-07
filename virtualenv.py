87e6fe74dfc257803eaf95f42c01465e82f78a43
sdc-dev <marthen7@gmail.com>
2019-11-07 10:56:51 +0800

    virtualenv initialization

---
+++ virtualenv.py
@@ -0,0 +1,13 @@
+Virtual Environment Initialize
+
+virtualenv -p python3 marthen_python
+cd marthen_python
+source bin/activate
+
+FOR MAC
+
+virtualenv -p python3 mydashenv
+
+source ~/.bash_profile
+conda activate mydashenv
+conda deactivate
